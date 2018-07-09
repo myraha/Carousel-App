c97bc07d33862f9fddbbb872900b1be614d70e77
Myraha Harmon-Arias <myrahaa@gmail.com>
2018-07-09 06:24:23 -0400

    readme

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+//user can click two buttons
+//click next and they go forward
+//click backa nd go back
+//img source is cycling based on clicks
+//user can see image changes
+
+//array to hold image sources
 var carousel = ["images/uno.jpg","images/dos.jpg","images/tres.jpg","images/quattro.jpg","images/cinco.jpg"];
 
 $(document).ready(function(){
  var pictureIndex = 0
+ //event listener
  $('#back').on("click", function(){
    if(pictureIndex == 0 ){
      pictureIndex = carousel.length - 1
@@ -10,8 +18,8 @@ $(document).ready(function(){
    }
    $('#imgCarousel').attr('src', carousel[pictureIndex])
  })
-
- $('#skip').on("click", function(){
+//event listener
+ $('#next').on("click", function(){
    if(pictureIndex == 4){
      pictureIndex = carousel.length - 5;
    }else{
